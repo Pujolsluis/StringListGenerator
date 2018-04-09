8b3243ee2fb86dba54e65dd5eedefbf6db5aa547
Luis Pujols <luispujols@hotmail.com>
2018-04-09 14:47:37 -0400

    Added the implementation of the list generator, now the program is completed

--- StringListGenerator/main.cpp
+++ StringListGenerator/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World" << endl;
+    vector<string> inputList;
+    string inputString;
+
+    while(cin >> inputString){
+        inputList.push_back(inputString);
+    }
+
+    // If you wish to sort the list after reading it uncomment the line below.
+    // sort(inputList.begin(), inputList.end());
+
+    cout << "---- DONE READING INPUT FILE ----" << endl;
+
+    for(string outputString : inputList) {
+        cout << "\"" << outputString << "\"," << endl;
+    }
+
+    cout << "---- DONE PRINTING OUTPUT ----" << endl;
+
     return 0;
 }
